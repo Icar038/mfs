dac47df975f570b3be0304e55e45f7519f19f3a9
Icar038 <icarosantanamagalhaes@gmail.com>
2025-02-05 16:04:54 -0300

    ajustando rota

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import Ladingpage from "./Paginas/Lading-page/LadingPage";
 function App() {
     const router = createBrowserRouter([
         {
-            path: "/mfs",
+            path: "/",
             element: <Ladingpage />,
             },
         {
